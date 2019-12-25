856b231fa838a5287df8aa2a86c26a8fe7623d4a
Coma <bmh4080@naver.com>
2019-12-25 19:29:39 +0900

    Create xmas.c

---
+++ xmas.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+        #define xmas (
+            #define xmaS )
+                #define xmAs {
+                    #define xmAS }
+                        #define xMas ;
+                           #define xMaS --
+                              #define xMAs int
+                                 #define xMAS main
+#define xma$ putc(Xm45 XMAs Xm4S XMAS, stdout)
+                           #define xMa$ >=
+       #define Xma$ putc(xm4S, stdout)
+#define Xmas putc(XM45, stdout)
+    #define XmaS ++
+        #define XmAs for
+            #define XmAS +=
+                #define XMas -=
+                    #define XMaS <=
+                        #define XMAs [
+                            #define XMAS ]
+
+xMAs xMAS xmas xmaS xmAs xMAs XM45 xMas XM45 XMas XM45 xMas
+XM45 XmaS xMas xMAs XMA5 xMas XMA5 XMas XMA5 xMas XMA5 XmAS
+XM45 xMas XM45 XmAS XmaS XM45 xMas xMAs XM4S xMas XM4S XMas
+XM4S xMas XM4S XmAS XM45 xMas XM4S xMaS xMas XM45 XmAS XmaS
+XM45 xMas xMAs xm45 xMas xm45 XMas xm45 xMas xm45 XmAS XM45
+xMas xMAs xm4S xMas xm4S XMas xm4S xMas xm4S XmAS XM45 xMas
+XM45 XmAS XmaS XM45 xMas XM45 XmAS XM45 xMas XM45 XmAS XM45
+xMas xMAs Xm45 XMAs XM4S XMAS xMas xMAs Xm4S xMas Xm4S XMas
+Xm4S xMas Xm45 XMAs Xm4S XMAS XMas Xm45 XMAs Xm4S XMAS xMas
+Xm45 XMAs Xm4S XMAS XmAS XM45 xMas XM45 XMas XmaS xm45 xMas
+Xm4S XmaS xMas Xm45 XMAs Xm4S XMAS XMas Xm45 XMAs Xm4S XMAS
+xMas Xm45 XMAs Xm4S XMAS XmAS XM45 xMas XM45 XMas XmaS xm45
+xMas Xm4S XmaS xMas Xm45 XMAs Xm4S XMAS XMas Xm45 XMAs Xm4S
+XMAS xMas Xm45 XMAs Xm4S XMAS XmAS XM45 xMas XM45 XmAS xMaS
+xm45 xMas xMaS xm45 xMas xMaS xm45 xMas xMaS xm45 xMas XM45
+XmAS xMaS xm45 xMas Xm4S XmaS xMas Xm45 XMAs Xm4S XMAS XMas
+Xm45 XMAs Xm4S XMAS xMas Xm45 XMAs Xm4S XMAS XmAS XM45 xMas
+Xm4S XMas Xm4S xMas XM45 XMas XM45 xMas XmaS XM45 xMas XM45
+XmAS XM45 xMas XM45 XmAS XM45 xMas XM45 XmAS XM45 xMas XM45
+XmAS XM45 xMas XM45 XmAS XM45 xMas xMAs xM45 xMas xMAs xM4S
+xMas XmAs xmas xMas XMA5 XMaS xm4S xMas XMA5 XmaS xmaS xmAs
+xM45 XMas xM45 xMas xM4S XMas xM4S xMaS xMas xM4S XmAS xm4S
+xMas xM4S XMas XMA5 xMas XmAs xmas xMas xM45 XMaS xM4S xMas
+xM45 XmaS xmaS xmAs Xmas xMas xmAS xM45 XMas xM45 xMas xM4S
+XMas xM4S xMas xM45 XmaS xMas xM4S XmAS XMA5 xMas xM4S XmAS
+XMA5 xMas xM4S xMaS xMas XmAs xmas xMas xM45 XMaS xM4S xMas
+xM45 XmaS xmaS xmAs xma$ xMas XmAs xmas xMas Xm4S xMa$ XM4S
+xMas Xm4S xMaS xmaS xmAs Xm4S XMas Xm4S xMas xmAS Xm4S XmaS
+xMas xmAS Xma$ xMas xmAS XMA5 XMas XMA5 xMas XMA5 XmAS xm4S
+xMas XMA5 XMas XM4S xMas XmAs xmas xMas Xm4S XMaS XM4S xMas
+Xm4S XmaS xmaS xmAs xm45 XMas xm45 xMas xm45 xMaS xMas XmAs
+xmas xMas xm45 XMaS XMA5 xMas xm45 XmaS xmaS xmAs Xmas xMas
+xmAS xma$ xMas Xma$ xMas xmAS xMas xMas xMas xMas xMas xmAS
