f5c7f3fb548bba451762b56b25f0b30df3238f31
Coma <bmh4080@naver.com>
2020-01-02 17:56:53 +0900

    Create xmas2.c

---
+++ xmas2.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+
+int main() {
+	int XM45;
+	XM45 -= XM45;
+	XM45++;
+	int XMA5;
+	XMA5 -= XMA5;
+	XMA5 += XM45;
+	XM45 += ++XM45;
+	int XM4S;
+	XM4S -= XM4S;
+	XM4S += XM45;
+	XM4S--;
+	XM45 += ++XM45;
+	int xm45;
+	xm45 -= xm45;
+	xm45 += XM45;
+	int xm4S;
+	xm4S -= xm4S;
+	xm4S += XM45;
+	XM45 += ++XM45;
+	XM45 += XM45;
+	XM45 += XM45;
+	int Xm45[XM4S];
+	int Xm4S;
+	Xm4S -= Xm4S;
+	Xm45[Xm4S] -= Xm45[Xm4S];
+	Xm45[Xm4S] += XM45;
+	XM45 -= ++xm45;
+	Xm4S++;
+	Xm45[Xm4S] -= Xm45[Xm4S];
+	Xm45[Xm4S] += XM45;
+	XM45 -= ++xm45;
+	Xm4S++;
+	Xm45[Xm4S] -= Xm45[Xm4S];
+	Xm45[Xm4S] += XM45;
+	XM45 += --xm45;
+	--xm45;
+	--xm45;
+	--xm45;
+	XM45 += --xm45;
+	Xm4S++;
+	Xm45[Xm4S] -= Xm45[Xm4S];
+	Xm45[Xm4S] += XM45;
+	Xm4S -= Xm4S;
+	XM45 -= XM45;
+	++XM45;
+	XM45 += XM45;
+	XM45 += XM45;
+	XM45 += XM45;
+	XM45 += XM45;
+	XM45 += XM45;
+	int xM45;
+	int xM4S;
+	for (; XMA5 <= xm4S; XMA5++) {
+		xM45 -= xM45;
+		xM4S -= xM4S--;
+		xM4S += xm4S;
+		xM4S -= XMA5;
+		for (; xM45 <= xM4S; xM45++) {
+			putc(XM45, stdout );
+		}
+		xM45 -= xM45;
+		xM4S -= xM4S;
+		xM45++;
+		xM4S += XMA5;
+		xM4S += XMA5;
+		xM4S--;
+		for (; xM45 <= xM4S; xM45++) {
+			putc(Xm45[Xm4S], stdout);
+			for (; Xm4S >= XM4S; Xm4S--) {
+				Xm4S -= Xm4S;
+			}
+			Xm4S++;
+		}
+		putc(xm4S, stdout);
+	}
+	XMA5 -= XMA5;
+	XMA5 += xm4S;
+	XMA5 -= XM4S;
+	for (; Xm4S <= XM4S; Xm4S++) {
+		xm45 -= xm45;
+		xm45--;
+		for (; xm45 <= XMA5; xm45++) {
+			putc(XM45, stdout);
+		}
+		putc(Xm45[Xm4S], stdout);
+		putc(xm4S, stdout);
+	}
+	;
+	;
+	;
+	;
+	;
+}
